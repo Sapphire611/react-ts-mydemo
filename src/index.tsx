a396e2e3b9b86c65d68dfe31aacef350a20e5818
Liu Liyi <liyi.liu@carota.ai>
2023-10-07 14:20:21 +0800

    取消严格模式

--- src/index.tsx
+++ src/index.tsx
@@ -7,8 +7,9 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+// 不取消严格模式，useEffect中的代码会执行2次 = =
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>
+  // </React.StrictMode>
 );
